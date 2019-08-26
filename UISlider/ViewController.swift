6d7c9329e12c3a59efa68c23ddd9531f030f7275
mohamedosama2074 <iosengks74@gmail.com>
2019-08-26 09:01:34 +0200

    Initial Commit

---
+++ UISlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UISlider
+//
+//  Created by Mohamed on 8/26/19.
+//  Copyright © 2019 Mohamed74. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
